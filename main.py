a3107a8016f7edac86aa895939efac72ab101eae
ArthurCarvalho2024 <carvalhocostarthur@gmail.com>
2025-02-15 01:01:13 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# DESAFIO
+
+# MISSÃO 1 - Restaurando as Regras Escolares
+# nota_aluno1 = 6
+# if nota_aluno >= 6:
+#     print("Aprovado")
+# else:
+#     print("Reprovado")
+# #------------------------------------------
+
+# MISSÃO 2 - O Sistema Eleitoral Secreto
+# sistema_votação = input("Digite a sua idade: ")
+# if int(sistema_votação) >= 16:
+#     print("Você pode votar")
+# else:
+#     print("Você ainda não pode votar") 
+#-------------------------------------------
+
+# MISSÃO 3 - Recuperando o Sistema de Notas
+# nota_aluno2 = 60
+# if nota_aluno2 >= 90 and nota_aluno2 < 101:
+#     print("Parabéns, você tirou A!")
+# elif nota_aluno2 >= 80 and nota_aluno2 < 90:
+#     print("Muito bem, você tirou B.")
+# elif nota_aluno2 >= 70 and nota_aluno2 < 80:
+#     print("Bom trabalho, você tirou C.")
+# elif nota_aluno2 >= 60 and nota_aluno2 < 70:
+#     print("Fique atento, você tirou D.")
+# elif nota_aluno2 < 60:
+#     print("Estude um pouco mais, você tirou F.")
+#-----------------------------------------------
+
+# MISSÃO 4 - Restaurando a Identificação de Números
+calculo1 = int(input("Escreva o primeiro número: "))
+calculo2 = int(input("Escreva o segundo número: "))
+soma = calculo1 + calculo2
+print(f"A soma dos números é: {soma}")
+#--------------------------------------------------
